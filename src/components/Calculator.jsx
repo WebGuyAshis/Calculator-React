e6875a6be09d3cfcb9f6a0b5d4f5e35364895019
Ashis <developer.ashis99@gmail.com>
2023-08-10 22:30:17 +0530

    added features

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,17 +1,83 @@
-import React from 'react';
-import '../assets/css/calculator.css'
+import React, { useState } from "react";
+import "../assets/css/calculator.css";
+import * as math from "mathjs";
 export default function Calculator() {
-    const keys = [1,2,3,"+",4,5,6,"-",7,8,9,"*",0,"DEL","AC","/", "="];
-  return (
-    <div className='calculator'>
-        <div className="display">
+  const keys = [
+    "+/-","%","AC","DEL",
+    7,8,9,"/",
+    4,5,6,"*",
+    1,2,3,"-",
+    0, ".","+","=",
+  ];
 
-        </div>
-        <div className="numpad">
-            {keys.map((btn)=>{
-               return <button value={btn}>{btn}</button>
-            })}
-        </div>
+  const signs = ["+", "-", "*", "/", "%"];
+
+  const [result, setResult] = useState("0");
+  const [expression, setExpression] = useState("");
+
+  const clickHandler = (e) => {
+    let value = e.target.value;
+
+    if (value === "AC") {
+      setExpression("");
+      setResult("0");
+    } else if (value === "DEL") {
+      setExpression(expression.slice(0, -1));
+      setResult("");
+    } else if (value === "=") {
+      try {
+        if (isNaN(result)) {
+          setResult("Error!");
+        }
+        setResult(math.evaluate(expression));
+      } catch (err) {
+        // setExpression("");
+        setResult("Error!");
+      }
+    } else if(value === "+/-"){
+      setResult(result * (-1))
+    }else if (signs.includes(value)) {
+      console.log(signs.includes(value));
+
+      let lastChar = expression.slice(-1);
+      console.log(lastChar);
+      if(lastChar === ""){
+        setExpression(0 + value)
+      setResult("");
+
+      }else
+      if (signs.includes(lastChar)) {
+        console.log(signs.includes(lastChar));
+        setExpression(expression.slice(0, -1) + value);
+      setResult("");
+
+      }else{
+      setExpression(expression + value);
+      setResult("");
+
+      }
+    }else {
+      setExpression(expression + value);
+      setResult("");
+
+    }
+  };
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="expression">{expression}</div>
+        <div className="result">{result}</div>
+      </div>
+      <hr />
+      <div className="numpad">
+        {keys.map((btn, key) => {
+          return (
+            <button key={key} value={btn} onClick={clickHandler}>
+              {btn}
+            </button>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
