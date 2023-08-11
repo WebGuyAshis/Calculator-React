9aacd155fe5b37dc98c66978a80394751e81d966
Ashis <developer.ashis99@gmail.com>
2023-08-11 17:30:29 +0530

    Fixed Bugs

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,21 +3,37 @@ import "../assets/css/calculator.css";
 import * as math from "mathjs";
 export default function Calculator() {
   const keys = [
-    "+/-","%","AC","DEL",
-    7,8,9,"/",
-    4,5,6,"*",
-    1,2,3,"-",
-    0, ".","+","=",
+    "+/-",
+    "%",
+    "AC",
+    "DEL",
+    7,
+    8,
+    9,
+    "/",
+    4,
+    5,
+    6,
+    "*",
+    1,
+    2,
+    3,
+    "-",
+    0,
+    ".",
+    "+",
+    "=",
   ];
 
   const signs = ["+", "-", "*", "/", "%"];
 
   const [result, setResult] = useState("0");
   const [expression, setExpression] = useState("");
+  const [evaluated, setEvaluated] = useState(false);
 
   const clickHandler = (e) => {
     let value = e.target.value;
-
+    console.log("Evaluated Init:", evaluated);
     if (value === "AC") {
       setExpression("");
       setResult("0");
@@ -25,41 +41,45 @@ export default function Calculator() {
       setExpression(expression.slice(0, -1));
       setResult("");
     } else if (value === "=") {
+      setEvaluated(true);
+      console.log("eval = :", evaluated);
       try {
         if (isNaN(result)) {
           setResult("Error!");
         }
         setResult(math.evaluate(expression));
       } catch (err) {
-        // setExpression("");
         setResult("Error!");
       }
-    } else if(value === "+/-"){
-      setResult(result * (-1))
-    }else if (signs.includes(value)) {
-      console.log(signs.includes(value));
-
+    } else if (value === "+/-") {
+      setResult(result * -1);
+    } else if (signs.includes(value)) {
       let lastChar = expression.slice(-1);
-      console.log(lastChar);
-      if(lastChar === ""){
-        setExpression(0 + value)
-      setResult("");
-
-      }else
-      if (signs.includes(lastChar)) {
-        console.log(signs.includes(lastChar));
+      if (lastChar === "") {
+        setExpression(0 + value);
+        setResult("");
+      } else if (evaluated) {
+        setExpression(result + value);
+        setResult("");
+      } else if (signs.includes(lastChar)) {
         setExpression(expression.slice(0, -1) + value);
-      setResult("");
-
-      }else{
-      setExpression(expression + value);
-      setResult("");
-
+        setResult("");
+      } else {
+        setExpression(expression + value);
+        setResult("");
       }
-    }else {
-      setExpression(expression + value);
-      setResult("");
-
+      setEvaluated(false);
+    } else {
+      console.log("Evaluate Before NUM", evaluated);
+      if (evaluated) {
+        console.log("Evaluated");
+        setEvaluated(false);
+        setExpression(value);
+        setResult("");
+      } else {
+        setExpression(expression + value);
+        setResult("");
+      }
     }
   };
   return (
